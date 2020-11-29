2043d0d58a6cd05174007e9620514df42f575baf
narwic <carwinyoung@gmail.com>
2020-11-29 04:12:23 -0600

    Move settings into their own file so we don't get stuck in weird export availability hell.

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Houses project settings.
+ */
+/**
+ * The project settings for the current run of the Flag generator.
+ *
+ * @type {{flagHeight: number, seed: boolean, flagWidth: number}}
+ */
+let settings = {
+    seed: false,
+    flagWidth: 500,
+    flagHeight: 300,
+}
+
+export default settings;
+
+// Complimentary Divisions
+// - a Border and a Pall look pretty good together.
